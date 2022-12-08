4b8d7a74732ab7c9278aa7c853e1242fed93783d
quibie <johann@fischi.xyz>
2022-12-08 17:06:37 +0100

    dumped all races into another file. post the initial message with a placeholder racestring to the first thread

--- client.py
+++ client.py
@@ -1,87 +1,87 @@
 #bot
-
+import dragonraces
 import os
-
+from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
-client = discord.Client(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-RacesWakingShore = """Advanced | Apex Canopy River Run / Flusslauf des Hohen Blätterdachs
-Advanced | Emberflow Flight / Glutstrom-Flug
-Advanced | Flashfrost Flyover / Blitzfrost-Überflug
-Advanced | Ruby Lifeshrine Loop / Rubinlebensschrein-Schleife
-Advanced | Uktulut Coaster / Uktuluter Küstenachter
-Advanced | Wild Preserve Circuit / Wildreservat-Überflug
-Advanced | Wild Preserve Slalom / Wildreservat-Slalom
-Advanced | Wingrest Roundabout / Schwingenrastkreisel
-Basic | Apex Canopy River Run / Flusslauf des Hohen Blätterdachs
-Basic | Emberflow Flight / Glutstrom-Flug
-Basic | Flashfrost Flyover / Blitzfrost-Überflug
-Basic | Ruby Lifeshrine Loop / Rubinlebensschrein-Schleife
-Basic | Uktulut Coaster / Uktuluter Küstenachter
-Basic | Wild Preserve Circuit / Wildreservat-Überflug
-Basic | Wild Preserve Slalom / Wildreservat-Slalom
-Basic | Wingrest Roundabout / Schwingenrastkreisel""".splitlines()
+Lraces = dragonraces.Races
 
 
 TestRaceString = """1. Platz: \"Rainaa | Johann\"\t - 13,37 Sekunden
-2. Platz: \"Djarin | Paddy\"\t - 14,45 Sekunden
+2. Platz:
 3. Platz:
 4. Platz:
 5. Platz:"""
-class race:
-    #the race class to track the times and create the message
-    def __init__(self, raceList, location):
-        self.loc = location
-        self.top5 = raceList.splitlines()
-        for i in range(5):
-            self.top5[i] = self.top5[i][10:]
 
 
-@client.event
+@bot.event
 async def on_ready():
     #get configuired guild
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
     
-    channel = client.get_channel(int(os.getenv('CHANNEL_ID')))
+    baseChannel = bot.get_channel(CHANNEL_ID)
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
     #init check if highscore list is there
-    for location in channel.threads:
+    for location in baseChannel.threads:
         counter = 0
-        async for msg in location.history(limit=200):
-            if msg.author == client.user:
-                counter +=1
-                print(f'Nachricht Nr.{counter}: {msg.content}\n')
-        if counter > 0:
-            print(f'Anzahl der Nachrichten: {counter} \n')
-        else:
-            await location.send(content='meine erste Nachricht durch code')
+        if "Shores" in location.name:
+            async for msg in location.history(limit=20):
+                if (msg.author == bot.user) and (any(i in Lraces[:15] for i in msg.content)):
+                    return
+                else:
+                    for i in range(15):
+                        await location.send(content=f'{Lraces[i]}\n{TestRaceString}')
+
+        # elif "Plains" in location.name:
+
+        # elif "Azure" in location.name:
+
+        # elif "Thaldaszus" in location.name:
+
+        # async for msg in location.history(limit=200):
+        #     if msg.author == bot.user:
+        #         counter +=1
+        # if counter > 0:
+        #     print(f'Anzahl der Nachrichten: {counter}')
+        # else:
+        #     await location.send(content='meine erste Nachricht durch code')
 
     #init create 4 threads
-    if len(channel.threads) == 0:
+    if len(baseChannel.threads) == 0:
        for threadName in ['Waking Shores', 'Ohn\'ahran Plains', 'Azure Span', 'Thaldaszus']:
-            await channel.create_thread(name=threadName,type=discord.ChannelType.public_thread)
-            print(f'created Thread {threadName}\n')
+            await baseChannel.create_thread(name=threadName,type=discord.ChannelType.public_thread)
+            print(f'created Thread {threadName}')
     else:
         print(f'all thread are already created')
 
+@bot.event
+async def on_message(message):
+    #check for message in the configured textChannel
+    if (message.channel.id != CHANNEL_ID) and (message.author == bot.user):
+        return
+    else:
+        await bot.process_commands(message)
 
-    TestRace = race(TestRaceString, "Discord Schleife")
+@bot.command()
+async def race(ctx, race: int, time: float):
+    print(f'LOG - Author: {ctx.author} Text: {ctx.message.content}')
+    await ctx.send(content=f'Hello \"{ctx.author.nick}\". Your time for race \"{Lraces[race -1]}\" is {time} seconds')
 
-    print(f'{TestRace.top5}\n')
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
---
+++ dragonraces.py
@@ -0,0 +1,55 @@
+Races = """Basic | Ruby Lifeshrine Loop / Rubinlebensschrein-Schleife
+Advanced | Ruby Lifeshrine Loop / Rubinlebensschrein-Schleife
+Basic | Wild Preserve Slalom / Wildreservat-Slalom
+Advanced | Wild Preserve Slalom / Wildreservat-Slalom
+Basic | Emberflow Flight / Glutstrom-Flug
+Advanced | Emberflow Flight / Glutstrom-Flug
+Basic | Apex Canopy River Run / Flusslauf des Hohen Blätterdachs
+Advanced | Apex Canopy River Run / Flusslauf des Hohen Blätterdachs
+Basic | Uktulut Coaster / Uktuluter Küstenachter
+Advanced | Uktulut Coaster / Uktuluter Küstenachter
+Basic | Wingrest Roundabout / Schwingenrastkreisel
+Advanced | Wingrest Roundabout / Schwingenrastkreisel
+Basic | Flashfrost Flyover / Blitzfrost-Überflug
+Advanced | Flashfrost Flyover / Blitzfrost-Überflug
+Basic | Wild Preserve Circuit / Wildreservat-Überflug
+Advanced | Wild Preserve Circuit / Wildreservat-Überflug
+Basic | Sundapple Copse Circuit / Sonnentüpfelhainstrecke
+Advanced | Sundapple Copse Circuit / Sonnentüpfelhainstrecke
+Basic | Fen Flythrough / Moor-Durchflug
+Advanced | Fen Flythrough / Moor-Durchflug
+Basic | Ravine River Run / Schluchtflusslauf
+Advanced | Ravine River Run / Schluchtflusslauf
+Basic | Emerald Gardens Ascent / Smaragdgartenaufstieg
+Advanced | Emerald Gardens Ascent / Smaragdgartenaufstieg
+Basic | Maruuki Dash / Maruukaispurt
+Advanced | Maruuki Dash / Maruukaispurt
+Basic | Mirror of the Sky Dash / Spurt zum Spiegel des Himmels
+Advanced | Mirror of the Sky Dash / Spurt zum Spiegel des Himmels
+Basic | River Rapids Route / Stromschnellenstrecke
+Advanced | River Rapids Route / Stromschnellenstrecke
+Basic | Azure Span Spring / Sprint des Azurblauen Gebirges
+Advanced | Azure Span Spring / Sprint des Azurblauen Gebirges
+Basic | Azure Span Slalom / Slalom des Azurblauen Gebirges
+Advanced | Azure Span Slalom / Slalom des Azurblauen Gebirges
+Basic | Vakthros Ascent / Vakthrosaufstieg
+Advanced | Vakthros Ascent / Vakthrosaufstieg
+Basic | Iskaara Tour / Iskaara-Tour
+Advanced | Iskaara Tour / Iskaara-Tour
+Basic | Frostland Flyover / Frostland-Überflug
+Advanced | Frostland Flyover / Frostland-Überflug
+Basic | Archive Ambit / Archivring
+Advanced | Archive Ambit / Archivring
+Basic | Flowing Forest Flight / Flug des Wallenden Walds
+Advanced | Flowing Forest Flight / Flug des Wallenden Walds
+Basic | Tyrhold Trial / Tyrhold-Tournee
+Advanced | Tyrhold Trial / Tyrhold-Tournee
+Basic | Cliffside Circuit / Klippenrundstrecke
+Advanced | Cliffside Circuit / Klippenrundstrecke
+Basic | Academy Ascent / Akademieaufstieg
+Advanced | Academy Ascent / Akademieaufstieg
+Basic | Garden Gallivant / Gartenpromenade
+Advanced | Garden Gallivant / Gartenpromenade
+Basic | Caverns Criss-Cross / Kreuz und quer durch die Höhlen
+Advanced | Caverns Criss-Cross / Kreuz und quer durch die Höhlen
+""".splitlines()
